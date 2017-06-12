fcd2f62f76b4213de38c026a9f895354705599fe
panter <panter2804@gmail.com>
2017-06-12 17:21:49 +0200

    Started Work on a Controll Function for the SN74HC595, for a later Multiplexing function for an led matrix

---
+++ F334DISCO/F3348_Multiplexing/src/main.c
@@ -0,0 +1,63 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f3xx.h"
+#include "stm32f3348_discovery.h"
+
+
+
+void Initializegpio()
+{
+	HAL_Init();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.Pin = GPIO_PIN_All;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
+    GPIO_InitStructure.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
+
+int main(void)
+{
+	Initializegpio();
+
+	for(;;) {
+
+	}
+}
+
+void Multiplex(/*uint8_t*/ unsigned char value) {
+
+	for (int i = 0; i < 8; ++i) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+		HAL_Delay(1);
+		if (value & 0x1) {
+			//puts("1");
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+			HAL_Delay(1);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+
+		} else {
+			//puts("0");
+			HAL_Delay(1);
+		}
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
+		HAL_Delay(1);
+	}
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+	value >>= 1;
+
+}

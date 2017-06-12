d75aeb568ad8f7bb13ae089cf5089a9558fbfbdb
panter <panter2804@gmail.com>
2017-06-12 22:38:31 +0200

    No big changens, Multiplexing still doesn't work.....

--- F334DISCO/F3348_Multiplexing/src/main.c
+++ F334DISCO/F3348_Multiplexing/src/main.c
@@ -11,7 +11,7 @@
 
 #include "stm32f3xx.h"
 #include "stm32f3348_discovery.h"
-
+char test = 10;
 
 
 void Initializegpio()
@@ -20,21 +20,44 @@ void Initializegpio()
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
 
-    GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.Pin = GPIO_PIN_All;
-    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
-    GPIO_InitStructure.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_InitTypeDef GPIO_InitStructure0;
+    GPIO_InitStructure0.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
+    GPIO_InitStructure0.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure0.Speed = GPIO_SPEED_HIGH;
+    GPIO_InitStructure0.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStructure0);
+
+    GPIO_InitTypeDef GPIO_InitStructure1;
+    GPIO_InitStructure1.Pin = GPIO_PIN_All;
+    GPIO_InitStructure1.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure1.Speed = GPIO_SPEED_HIGH;
+    GPIO_InitStructure1.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStructure1);
+
 }
 
 int main(void)
 {
-	Initializegpio();
-
+	/*Initializegpio();
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+	test++;
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+	test++;
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	test++;
+	HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_2);
+	test++;
+	HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_2);
+	test++;
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	test++;
+*/
 	for(;;) {
-
+     // test++;
+      Multiplex(test);
+      HAL_Delay(1000);
 	}
+	HAL_DeInit();
 }
 
 void Multiplex(/*uint8_t*/ unsigned char value) {
@@ -45,6 +68,7 @@ void Multiplex(/*uint8_t*/ unsigned char value) {
 		if (value & 0x1) {
 			//puts("1");
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+
 			HAL_Delay(1);
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
 

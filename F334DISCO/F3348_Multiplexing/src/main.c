358365938eac0594219477146d32b8ab68beed76
panter <panter2804@gmail.com>
2017-06-13 17:03:02 +0200

    first working version for driving SN74HC595 8-bit shift register

--- F334DISCO/F3348_Multiplexing/src/main.c
+++ F334DISCO/F3348_Multiplexing/src/main.c
@@ -11,7 +11,14 @@
 
 #include "stm32f3xx.h"
 #include "stm32f3348_discovery.h"
-char test = 1;
+
+#define SER		GPIO_PIN_0
+#define RCLK	GPIO_PIN_1
+#define SRCLK	GPIO_PIN_2
+
+#define DELAY	10
+
+unsigned char test = 0xF0;
 
 
 void Initializegpio()
@@ -21,7 +28,7 @@ void Initializegpio()
     __HAL_RCC_GPIOB_CLK_ENABLE();
 
     GPIO_InitTypeDef GPIO_InitStructure0;
-    GPIO_InitStructure0.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
+    GPIO_InitStructure0.Pin = SER | RCLK | SRCLK;
     GPIO_InitStructure0.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStructure0.Speed = GPIO_SPEED_HIGH;
     GPIO_InitStructure0.Pull = GPIO_NOPULL;
@@ -34,22 +41,24 @@ void Initializegpio()
     GPIO_InitStructure1.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStructure1);
 
+    HAL_GPIO_WritePin(GPIOA, RCLK, GPIO_PIN_SET);
+
 }
 
 int main(void)
 {
 	Initializegpio();
-	/*HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+	/*HAL_GPIO_WritePin(GPIOB, SRCLK, GPIO_PIN_SET);
 	test++;
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
 	test++;
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, SRCLK, GPIO_PIN_RESET);
 	test++;
-	HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_2);
+	HAL_GPIO_TogglePin(GPIOB,SRCLK);
 	test++;
-	HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_2);
+	HAL_GPIO_TogglePin(GPIOB,SRCLK);
 	test++;
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, SRCLK, GPIO_PIN_RESET);
 	test++;
 */
 	for(;;) {
@@ -61,28 +70,27 @@ int main(void)
 }
 
 void Multiplex(/*uint8_t*/ unsigned char value) {
-
 	for (int i = 0; i < 8; ++i) {
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
-		HAL_Delay(1);
+		HAL_GPIO_WritePin(GPIOA, SRCLK, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA, RCLK, GPIO_PIN_RESET);
+		HAL_Delay(DELAY);
 		if (value & 0x1) {
 			//puts("1");
-			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
-
-			HAL_Delay(1);
-			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOA, SER, GPIO_PIN_SET);
+			HAL_Delay(DELAY);
+			HAL_GPIO_WritePin(GPIOA, SRCLK, GPIO_PIN_SET);
 
 		} else {
 			//puts("0");
-			HAL_Delay(1);
-			value >>= 1;
+			HAL_GPIO_WritePin(GPIOA, SER, GPIO_PIN_RESET);
+			HAL_Delay(DELAY);
+			HAL_GPIO_WritePin(GPIOA, SRCLK, GPIO_PIN_SET);
 		}
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
-		HAL_Delay(1);
+		HAL_Delay(DELAY);
+		HAL_GPIO_WritePin(GPIOA, RCLK, GPIO_PIN_SET);
+		HAL_Delay(DELAY);
+
+		value >>= 1;
 	}
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
-	HAL_Delay(1);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
-
-
+	HAL_GPIO_WritePin(GPIOA, RCLK, GPIO_PIN_SET);
 }
--- F334DISCO/f476_lcd/src/main.c
+++ F334DISCO/f476_lcd/src/main.c
@@ -26,72 +26,10 @@ int main(void) {
 		BSP_LCD_GLASS_DisplayString((uint8_t*) "Hello");
 		HAL_Delay(2000);
 		x++;
-		BSP_LCD_GLASS_Clear();
-		switch (x) {
-		case 1:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "1");
-			break;
-		case 2:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "2");
-			break;
-		case 3:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "tree");
-			break;
-		case 4:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "4");
-			break;
-		case 5:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "Five");
-			break;
-		case 6:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "Sechs");
-			break;
-		case 7:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "Sept");
-			break;
-		case 8:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "Hello");
-			break;
-		case 9:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "1");
-			break;
-		case 2:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "2");
-			break;
-		case 10:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "1");
-			break;
-		case 2:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "2");
-			break;
-		case 11:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "1");
-			break;
-		case 12:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "2");
-			break;
-		case 13:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "1");
-			break;
-		case 14:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "2");
-			break;
-		case 15:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "1");
-			break;
-		case 16:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "2");
-			break;
-		case 17:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "1");
-			break;
-		case 18:
-			BSP_LCD_GLASS_DisplayString((uint8_t*) "2");
-			break;
-
-		}
-		HAL_Delay(2000);
-
+	/*	BSP_LCD_GLASS_Clear();
+		BSP_LCD_GLASS_DisplayStrDeci(x);
+		HAL_Delay(500);
+*/
 	}
 	BSP_LCD_GLASS_DeInit();
 	HAL_DeInit();
